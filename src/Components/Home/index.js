3e22472ab6b8765cedcc112b474b6bfb252012c4
phanthaoIT <ptthao.itus@gmail.com>
2018-11-30 22:18:47 +0700

    add header

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Topbar from '../Topbar'
 import Profile from '../Profile'
 import Post from '../Post'
+import Header from '../Header'
 const Home = (props) => {
   document.title = 'Home'
   return (
     <React.Fragment>
       <Topbar/>
+      <Header/>
       <div style={{display: "flex"}}>
       <Profile/>
       <Post/>
