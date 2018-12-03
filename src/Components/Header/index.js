91c0ecb755d3ea29766b211af90c289c57e1acc0
1512500 <thanhvantran0@gmail.com>
2018-12-03 11:36:57 +0700

    Cập nhật phần header profile, thêm nút edit, chuyển trạng thái khi click

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,34 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCamera } from '@fortawesome/free-solid-svg-icons'
-const Header = (props) => {
-return (
-  <div className="Header">
-      <div className="Header-Photo" style={{width:'100%', height: '320px'}} >
-        <FontAwesomeIcon icon={faCamera}/>
-      </div>
-      <div className="bar">
-        <ul className="Arrange" style={{marginLeft:'30%'}}>
-          <li className="ArrangeSizeFit">
-            <a title="9.840 Tweet">
-              <span className="StatLabel">Tweets</span>
-              <span className="StatValue">9.840</span>
-            </a>
-          </li>
-          <li className="ArrangeSizeFit">
-            <a  title="885 Following">
-              <span className="StatLabel">Following</span>
-              <span className="StatValue">885</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className='big_avatar'>
 
+const Header = (props) => {
+  let [editProfile, setEditProfile] = useState(false);
+  return (
+    <div className="Header">
+      {editProfile? <div className="Header-Photo" style={{ width: '100%', height: '320px' }} >
+        <div className="add-photo-container">
+          <FontAwesomeIcon icon={faCamera} />
+          <h4 className="add_photo">Add a header photo</h4>
+        </div>
+      </div> :
+      <div className="Header-Photo-NoEdit" style={{ width: '100%', height: '320px' }} >
       </div>
-  </div>
-)
+      }
+      
+      <div className="bar">
+        <div className="follow-container">
+
+        </div>
+        <div className="button-container">
+          {!editProfile ?
+            <button onClick={()=> setEditProfile(true)}>Edit</button> :
+            <React.Fragment>
+              <button onClick={()=> setEditProfile(false)}>Cancel</button>
+              <button>Save changes</button>
+            </React.Fragment>
+          }
+        </div>
+      </div>
+
+      {editProfile ?  <div className='big_avatar'>
+        <div className="add-profile-container">
+          <FontAwesomeIcon icon={faCamera} />
+          <h4 className="add-profile-photo">Add a profile photo</h4>
+        </div>
+      </div>:  <div className='big-avatar-NoEdit'>
+        <div className="add-profile-container-NoEdit">
+          <FontAwesomeIcon icon={faCamera} />
+        </div>
+      </div>}
+     
+    </div>
+  )
 }
 
 export default Header
